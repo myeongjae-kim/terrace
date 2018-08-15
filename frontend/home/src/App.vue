2dab4d42d4b91eca0d5979a7dcbc0b53488217e8
hrzon <myeongjae.kim@icloud.com>
2018-08-15 15:50:16 -0500

    Fonts are moved to my server.

--- frontend/home/src/App.vue
+++ frontend/home/src/App.vue
@@ -20,7 +20,7 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700|Inconsolata');
-@import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
+@import url('/css/SpoqaHanSans.css');
 
 #app {
 	font-family: 'Source Sans Pro', 'Spoqa Han Sans', Helvetica, Arial, sans-serif;
