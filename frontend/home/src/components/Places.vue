7309550bf40405bd4c24ac41453d4cf7a5a5cfc9
hrzon <myeongjae.kim@icloud.com>
2018-08-18 17:08:29 -0500

    mapbox javascript is removed and the map is added by iframe. This removes size of javascript codes.

--- frontend/home/src/components/Places.vue
+++ frontend/home/src/components/Places.vue
@@ -3,31 +3,15 @@
     <h1>Places</h1>
     <p>where I have been</p>
     <div id="map-wrapper">
-      <div id="map"></div>
+      <iframe id="map" src="https://api.mapbox.com/styles/v1/myeongjae/cjl07pcz14j9t2sqmsp0swqhg.html?fresh=true&title=true&access_token=pk.eyJ1IjoibXllb25namFlIiwiYSI6ImNqbDAzdWFhZjEwd2kza3Bncmo0emFtM2wifQ.j2Y4BLsTivJxT7BU_bWFKg#1.0/34.307156/128.181610/0"> </iframe>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Places',
-  mounted: function() {
-    loadMap();
-  }
+  name: 'Places'
 };
-
-import mapboxgl from 'mapbox-gl';
-
-function loadMap() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoibXllb25namFlIiwiYSI6ImNqbDAzdWFhZjEwd2kza3Bncmo0emFtM2wifQ.j2Y4BLsTivJxT7BU_bWFKg';
-
-  // eslint-disable-next-line
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/myeongjae/cjl07pcz14j9t2sqmsp0swqhg',
-  });
-}
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -38,6 +22,8 @@ function loadMap() {
 
 #map {
   height: 500px;
+  width: 100%;
   text-align: justify;
+  border: 0
 }
 </style>
