fc78bf8e4889f0aacadc4f6760e667075de3e19d
hrzon <zods@me.com>
2018-09-25 12:46:33 +0900

    Blog.vue is reverted to original.

--- frontend/home/src/components/Blog.vue
+++ frontend/home/src/components/Blog.vue
@@ -19,9 +19,9 @@
         <div id="padding-between-title-and-article"></div>
         <div id="article-content" v-html="article"></div>
         <div id="share-buttons">
-          <div class="copy-btn button" :data-clipboard-text="address">{{copyBtnMsg}}</div>
+          <button class="copy-btn" :data-clipboard-text="address">{{copyBtnMsg}}</button>
           &nbsp;
-          <router-link to="/blog/"><div class="button">Article List</div></router-link>
+          <router-link to="/blog/"><button>Article List</button></router-link>
         </div>
       </article>
       <div id="adjacent-articles">
@@ -461,10 +461,8 @@ article {
 }
 
 .button-clicked {
-  /*
   color: #fff;
   background-color: #2c3e50;
-  */
 }
 
 #adjacent-articles {
