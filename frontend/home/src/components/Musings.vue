ff9222e17c39efaa09f86fc954349416bc802905
hrzon <zods@me.com>
2018-10-20 20:38:03 +0900

    [Update] Update drafts

--- frontend/home/src/components/Musings.vue
+++ frontend/home/src/components/Musings.vue
@@ -65,7 +65,7 @@ export default {
         {
           key : 3,
           sentences : ["“It seems that perfection is attained not when there is nothing more to add, but when there is nothing more to remove."],
-          author : "- Antoine de Saint Exupéry, <Wind, Sand and Stars> Chapter 3",
+          author : "- Antoine de Saint Exupéry, <Wind, Sand and Stars>, 1939, Chapter 3",
           css : "font-family: 'Bad Script'; font-style:normal;",
         },
 
