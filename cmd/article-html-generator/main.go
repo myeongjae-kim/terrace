dbaeca00b75f335a2b63c551687791b6e70c6bee
hrzon <dev@myeongjae.kim>
2019-02-02 18:34:40 +0900

    [#5] Complete articles-metadata-injector

--- cmd/article-html-generator/main.go
+++ cmd/article-html-generator/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"github.com/myeongjae-kim/terrace/internal/pkg/contents"
-	"github.com/myeongjae-kim/terrace/internal/pkg/diriteration"
 )
 
 func main() {
-	diriteration.DirectoryRecursiveIteration(contents.ContentsRoot, contents.RemoveHTML)
-	diriteration.DirectoryRecursiveIteration(contents.ContentsRoot, contents.PandocMDtoHTML)
+	contents.RemoveHTML()
+	contents.PandocMDtoHTML()
 }
