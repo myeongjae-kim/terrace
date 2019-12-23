5016a3bcc67f1ab964045544633adf291801188b
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-23 15:33:44 +0900

    [#16] Remove horizontalMenuBar.

--- src/common/presentation/components/organisms/index.ts
+++ src/common/presentation/components/organisms/index.ts
@@ -2,4 +2,3 @@ export * from './NotificationCenter';
 
 export { default as MarkdownEditor } from './MarkdownEditor';
 export { default as MyTable } from './MyTable';
-export { default as HorizontalMenuBar } from './HorizontalMenuBar';
--- src/common/presentation/components/templates/MainLayout/MainLayout.tsx
+++ src/common/presentation/components/templates/MainLayout/MainLayout.tsx
@@ -2,10 +2,8 @@ import { createStyles, CssBaseline, makeStyles, Theme } from '@material-ui/core'
 import React from 'react';
 import { WithTranslation } from 'react-i18next';
 import I18NService from 'src/common/domain/service/I18NService';
-import HorizontalMenuBarContainer from '../../../container/molecules/HorizontalMenuBarContainer';
+import Footer from './Footer';
 import TopBar from './TopBar';
-
-const horizontalMenuBarHeight = 31; /* manually calculate the height of horizonMenuBar */
 
 const { withTranslation } = I18NService;
 
@@ -16,24 +14,9 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
       marginTop: theme.spacing(2)
     }
   },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-  },
-  horizontalMenuBarWrapper: {
-    position: 'fixed',
-    zIndex: theme.zIndex.drawer - 1,
-    width: "100%"
-  },
-  horizontalMenuBar: {
-    height: horizontalMenuBarHeight
   }
 }));
 
@@ -49,16 +32,10 @@ const MainLayout: React.FC<Props> = ({ children }) => {
     <div className={classes.rootContainer}>
       <CssBaseline />
       <TopBar />
-      <div style={{ width: "100%" }}>
-        <div className={classes.toolbar} />
-        <div className={classes.horizontalMenuBarWrapper}>
-          <HorizontalMenuBarContainer />
-        </div>
-        <main className={classes.content}>
-          <div className={classes.horizontalMenuBar} />
-          {children}
-        </main>
-      </div>
+      <main className={classes.content}>
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
