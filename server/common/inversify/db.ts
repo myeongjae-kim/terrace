652c1f1523f0c6819294c6aab89de878c0408063
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-25 16:50:27 +0900

    [#23] Add connection to db.

--- server/common/inversify/db.ts
+++ server/common/inversify/db.ts
@@ -1,35 +1,30 @@
 import { Musing } from 'src/musings/domain/model'
 import { createConnection } from "typeorm";
 
-/*
-Please set the env var:
-export DATABASE_USER=postgres \
-export DATABASE_PASSWORD=secret \
-export DATABASE_HOST=localhost \
-export DATABASE_PORT=3306 \
-export DATABASE_DB=demo
-*/
-
 export async function getDbConnection() {
-
-  const DATABASE_HOST = process.env.DATABASE_HOST || "localhost";
-  const DATABASE_USER = process.env.DATABASE_USER || "";
-  const DATABASE_PORT = 3306;
-  const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "";
-  const DATABASE_DB = "demo";
+  const {
+    DATABASE_HOST,
+    DATABASE_USER,
+    DATABASE_PORT,
+    DATABASE_PASSWORD,
+    DATABASE_DB,
+    PROFILE,
+  } = process.env;
 
   const entities = [
     Musing
   ];
 
   return await createConnection({
-    type: "postgres",
+    type: "mariadb",
     host: DATABASE_HOST,
-    port: DATABASE_PORT,
+    port: Number(DATABASE_PORT),
     username: DATABASE_USER,
     password: DATABASE_PASSWORD,
     database: DATABASE_DB,
     entities,
-    synchronize: true
+    synchronize: PROFILE === 'local',
+    supportBigNumbers: true,
+    bigNumberStrings: true
   });
 }
--- src/musings/domain/model/Musing.ts
+++ src/musings/domain/model/Musing.ts
@@ -3,18 +3,29 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity()
 export class Musing {
 
-  @PrimaryGeneratedColumn()
-  public id: number = -1;
+  public static from({ quote, from, language }: Omit<Musing, "id">) {
+    const obj = new Musing();
+    obj.quote = quote;
+    obj.from = from;
+    obj.language = language;
+
+    return obj;
+  }
+
+  @PrimaryGeneratedColumn({
+    type: 'bigint'
+  })
+  public id!: string;
 
   @Column({
     type: 'text'
   })
-  public quote: string = "";
+  public quote: string = '';
 
   @Column()
-  public from: string = "";
+  public from: string = '';
 
   @Column()
-  public language: 'EN' | 'KO' = 'EN'
+  public language: 'EN' | 'KO' = 'EN';
 
 }
