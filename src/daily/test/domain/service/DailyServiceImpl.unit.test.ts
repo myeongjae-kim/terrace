9ac34230e1d7fbd5db6955cd1d323dac8812ef0e
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-27 00:31:22 +0900

    [#20] Fix test.

--- src/daily/test/domain/service/DailyServiceImpl.unit.test.ts
+++ src/daily/test/domain/service/DailyServiceImpl.unit.test.ts
@@ -6,12 +6,12 @@ import { DailyService, DailyServiceImpl } from "src/daily/domain/service";
 import { createDailyFixture } from "../model/Daily.unit.test";
 
 describe('DailyServiceImpl', () => {
-  let dailyRepository: Pick<DailyRepository, "find">;
+  let dailyRepository: Pick<DailyRepository, "findAllByOrderBySeqDesc">;
   let dailyService: DailyService;
 
   beforeEach(() => {
     dailyRepository = {
-      find: jest.fn()
+      findAllByOrderBySeqDesc: jest.fn()
     }
 
     dailyService = new DailyServiceImpl(dailyRepository as DailyRepository);
@@ -20,7 +20,7 @@ describe('DailyServiceImpl', () => {
   it('should return found dailys.', async () => {
     // given
     const daily = createDailyFixture();
-    (dailyRepository.find as jest.Mock<Promise<Daily[]>>).mockResolvedValue([daily]);
+    (dailyRepository.findAllByOrderBySeqDesc as jest.Mock<Promise<Daily[]>>).mockResolvedValue([daily]);
 
     // when
     const result = await dailyService.findAll();
