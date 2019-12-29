c66aa75825e6017b1b611766b525131297b49f6a
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-12-29 20:48:58 +0900

    [#21] Add states for blog.

--- src/common/presentation/state-module/root.ts
+++ src/common/presentation/state-module/root.ts
@@ -1,6 +1,7 @@
 import { combineReducers, } from "redux";
 import { reducer as formReducer } from 'redux-form';
 import { fork } from "redux-saga/effects";
+import * as blogModule from "src/blog/presentation/state-modules"
 import * as dailyModule from "src/daily/presentation/state-modules"
 import * as motherModule from "src/mother/presentation/state-modules"
 import * as musingsModule from "src/musings/presentation/state-modules"
@@ -14,12 +15,14 @@ export const rootReducer = combineReducers({
   common: commonModule.reducer,
   snackbar: snackbarModule.reducer,
 
+  blog: blogModule.reducer,
   mother: motherModule.reducer,
   musings: musingsModule.reducer,
   daily: dailyModule.reducer,
 });
 
 export function* rootSaga() {
+  yield fork(blogModule.saga);
   yield fork(motherModule.saga);
   yield fork(musingsModule.saga);
   yield fork(dailyModule.saga);
