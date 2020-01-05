aba565f33b2d45989bc964be5fb2129bd387f002
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-06 01:19:49 +0900

    [#develop] Increase memory size of a container.

--- templates/4-myeongjaekim-ecs-service-cf-create-stack.sh
+++ templates/4-myeongjaekim-ecs-service-cf-create-stack.sh
@@ -2,7 +2,8 @@
 
 python3 4-myeongjaekim-ecs-service-cf-template.py > 4-myeongjaekim-ecs-service-cf.template
 
-awsecr cloudformation create-stack \
+# services are automatically created by pipeline.
+awsecr cloudformation update-stack \
 --stack-name myeongjaekim-staging-service \
 --capabilities CAPABILITY_IAM \
 --template-body file://4-myeongjaekim-ecs-service-cf.template \
--- templates/4-myeongjaekim-ecs-service-cf-template.py
+++ templates/4-myeongjaekim-ecs-service-cf-template.py
@@ -56,7 +56,7 @@ t.add_resource(TaskDefinition(
                 ImportValue("myeongjaekim-repo"),
                 ":",
                 Ref("Tag")]),
-            Memory=512,
+            Memory=1920,
             Cpu=2048,
             Name=Select(0, Split("-", Ref("AWS::StackName"))),
             Environment=[Environment(
