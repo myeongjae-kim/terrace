4570d1d93a922f8ce48e407846e91688ca0cf716
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-19 16:47:02 +0900

    [#54] Fix styles.

--- src/common/presentation/components/organisms/Comment.tsx
+++ src/common/presentation/components/organisms/Comment.tsx
@@ -2,34 +2,30 @@ import { NoSsr } from '@material-ui/core';
 import * as React from 'react';
 
 interface Props {
-  title: string
   identifier: string
-  url: string
 }
 
-const DisqusContent: React.FC<Props> = ({ title, identifier, url }) => {
+const Utterances: React.FC<Props> = ({ identifier }) => {
   React.useEffect(() => {
-    initDisqus('myeongjae-new', identifier, title, url)
+    initComment();
   }, [identifier])
 
-  return <div hidden={!identifier}>
-    <div id="utterances-container" style={{
-      margin: 'auto',
-      marginBottom: 20
-    }} />
-  </div>;
+  return <div id="comment-container" style={{
+    margin: 'auto',
+    marginBottom: 20
+  }} />;
 }
 
-const Disqus: React.SFC<Props> = (props) => {
+const Comment: React.FC<Props> = (props) => {
   return <NoSsr>
-    <DisqusContent {...props} />
+    <Utterances {...props} />
   </NoSsr>;
 }
 
-export default Disqus;
+export default Comment;
 
-function initDisqus(_: string, __: string, ___: string, ____: string) {
-  const utterancesContainer = document.querySelector("#utterances-container");
+const initComment = () => {
+  const utterancesContainer = document.querySelector("#comment-container");
   utterancesContainer?.childNodes.forEach(v => { v.remove() })
 
   const utterances = document.createElement('script');
