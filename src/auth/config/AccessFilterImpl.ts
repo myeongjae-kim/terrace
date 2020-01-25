b3037328017b2afba1a743b9044ee4462dba14b8
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-25 20:47:47 +0900

    [#56] Add request matchers to protect blog api.

--- src/auth/config/AccessFilterImpl.ts
+++ src/auth/config/AccessFilterImpl.ts
@@ -5,19 +5,18 @@ import Optional from 'optional-js';
 import { Endpoints } from "src/common/constants/Constants";
 import { UnauthorizedException } from "../exceptions";
 import { AccessFilter } from "./AccessFilter";
-
-interface RequestSummary {
-  method: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"
-  path: string
-}
+import { RequestMatcher } from './RequestMatcher';
 
 @injectable()
 export class AccessFilterImpl implements AccessFilter {
 
-  private readonly protectedRequests: RequestSummary[] = [{
-    method: "GET",
-    path: `${Endpoints.blog}/api`
-  }]
+  private readonly protectedRequests: RequestMatcher[] = [
+    new RequestMatcher("POST", `${Endpoints.blog}/api`),
+    new RequestMatcher("PUT", `${Endpoints.blog}/api/\\d+`),
+    new RequestMatcher("DELETE", `${Endpoints.blog}/api/\\d+`),
+    new RequestMatcher("PATCH", `${Endpoints.blog}/api/\\d+/publish`),
+    new RequestMatcher("PATCH", `${Endpoints.blog}/api/\\d+/unpublish`),
+  ]
 
   public filter = async (req: express.Request, principal?: interfaces.Principal): Promise<void> => {
     const authenticated = Optional.ofNullable(principal)
@@ -29,7 +28,7 @@ export class AccessFilterImpl implements AccessFilter {
     }
 
     this.protectedRequests.forEach(p => {
-      if (req.method === p.method && req.path === p.path) {
+      if (p.match(req)) {
         throw new UnauthorizedException();
       }
     })
