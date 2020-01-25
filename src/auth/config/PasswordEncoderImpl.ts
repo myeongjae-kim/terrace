54b57aa6cc6febe7994da96e01d670dc3c6c7e9b
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-25 11:27:37 +0900

    [#56] Short PasswordEncoderImpl codes.

--- src/auth/config/PasswordEncoderImpl.ts
+++ src/auth/config/PasswordEncoderImpl.ts
@@ -1,18 +1,11 @@
-// tslint:disable-next-line
-
 import bcrypt from "bcryptjs";
 import { injectable } from "inversify";
 import { PasswordEncoder } from ".";
 
 @injectable()
 export class BCryptPasswordEncoder implements PasswordEncoder {
+  public encode = (input: string): Promise<string> => bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(input, salt));
 
-  public encode = (input: string): Promise<string> => {
-    return bcrypt.genSalt(10)
-      .then(salt => bcrypt.hash(input, salt));
-  }
-
-  public match = (actual: string, encoded: string): Promise<boolean> => {
-    return bcrypt.compare(actual, encoded);
-  }
+  public match = (actual: string, encoded: string): Promise<boolean> => bcrypt.compare(actual, encoded);
 }
