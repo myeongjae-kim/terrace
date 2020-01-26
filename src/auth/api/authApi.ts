1257fd18ed4baf1bb034fbc1bb4f6091d64bde9a
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-01-26 18:20:36 +0900

    [#60] Implement sign-in page.

--- src/auth/api/authApi.ts
+++ src/auth/api/authApi.ts
@@ -1,10 +1,10 @@
 import Axios from "axios";
-// import { API_HOST, Endpoints } from "src/common/constants/Constants";
+import { API_HOST, Endpoints } from "src/common/constants/Constants";
 import CommonErrorServiceImpl from "src/common/infrastructure/service/CommonErrorServiceImpl";
 
 export const authApi = {
   signIn: (request: { email: string, password: string }): Promise<void> => new Promise((resolve, reject) => {
-    Axios.post<void>(`/auth/api/sign-in`, request)
+    Axios.post<void>(`${API_HOST}${Endpoints.auth}/api/sign-in`, request)
       .then(() => resolve())
       .catch(e => reject(CommonErrorServiceImpl.createRepositoryErrorFrom(e)));
   })
