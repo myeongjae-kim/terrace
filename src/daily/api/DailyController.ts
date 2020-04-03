c7e3e9cba83194a1773464fb6099ffc823e61bfe
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-04-03 23:04:43 +0900

    [#85] Add daily controller for create, update, remove.

--- src/daily/api/DailyController.ts
+++ src/daily/api/DailyController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { inject } from "inversify";
-import { controller, httpGet, interfaces, request, requestParam, response } from "inversify-express-utils";
+import { controller, httpGet, interfaces, request, requestParam, response, httpPost, requestBody, httpPut, httpDelete } from "inversify-express-utils";
 import { TYPES } from "server/common/inversify/types";
 import { NextApplication } from "server/common/nextjs/NextApplication";
 import { Endpoints } from "src/common/constants/Constants";
 import { DailyService } from "../domain/service";
+import { DailyDetailRequestDto } from "./dto/DailyDetailRequestDto";
+import { CreationResponse } from "src/common/api/dto/CreationResponse";
+import assert from "assert-plus";
 
 const PATH = Endpoints.daily;
 const PATH_DETAIL = Endpoints["daily.detail"];
@@ -27,6 +30,22 @@ export class DailyController implements interfaces.Controller {
     return this.nextApp.render(true, req, res, PATH_DETAIL);
   }
 
+  @httpPost("/api")
+  public async postDetail(
+    @requestBody() body: DailyDetailRequestDto
+  ): Promise<CreationResponse> {
+    assert.object(body, "body must be an object.");
+    assert.number(body.seq, "body.seq must be a number.");
+    assert.ok(body.title, "body.title must not be blank.");
+    assert.ok(body.content, "body.content must not be blank.");
+    assert.ok(body.slug, "body.slug must not be blank.");
+    assert.ok(!body.slug.includes("/"), "body.slug must not include slashes.");
+
+    return {
+      id: await this.dailyService.create(body)
+    };
+  }
+
   @httpGet("/api")
   public get() {
     return this.dailyService.findAll();
@@ -40,5 +59,33 @@ export class DailyController implements interfaces.Controller {
     @requestParam("slug") slug: string,
   ) {
     return this.dailyService.find({ year, month, day, slug });
+  }
+
+  @httpPut("/api/:year/:month/:day/:slug")
+  public editDetail(
+    @requestParam("year") year: string,
+    @requestParam("month") month: string,
+    @requestParam("day") day: string,
+    @requestParam("slug") slug: string,
+    @requestBody() body: DailyDetailRequestDto
+  ) {
+    assert.object(body, "body must be an object.");
+    assert.number(body.seq, "body.seq must be a number.");
+    assert.ok(body.title, "body.title must not be blank.");
+    assert.ok(body.content, "body.content must not be blank.");
+    assert.ok(body.slug, "body.slug must not be blank.");
+    assert.ok(!body.slug.includes("/"), "body.slug must not include slashes.");
+
+    return this.dailyService.update({ year, month, day, slug }, body);
+  }
+
+  @httpDelete("/api/:year/:month/:day/:slug")
+  public deleteDetail(
+    @requestParam("year") year: string,
+    @requestParam("month") month: string,
+    @requestParam("day") day: string,
+    @requestParam("slug") slug: string,
+  ) {
+    return this.dailyService.delete({ year, month, day, slug });
   }
 }
