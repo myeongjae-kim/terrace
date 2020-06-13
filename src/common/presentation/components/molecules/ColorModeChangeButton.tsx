cdf2bdd2d56d294f21ff6f5ce4946e7241ba1826
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-06-13 14:59:37 +0900

    [#94] Add mode change button.

---
+++ src/common/presentation/components/molecules/ColorModeChangeButton.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { IconButton, makeStyles, createStyles } from "@material-ui/core";
+import { Brightness5, Brightness4 } from "@material-ui/icons";
+
+interface Props {
+  isDark: boolean;
+  toggle(): void;
+}
+ 
+const useStyles = makeStyles(theme => createStyles({
+  icon: {
+    position: "fixed",
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+    opacity: 0.5,
+    zIndex: theme.zIndex.snackbar
+  }
+}));
+
+const ColorModeChangeButton: React.FC<Props> = ({isDark, toggle}) => {
+  const classes = useStyles();
+
+  return <IconButton className={classes.icon} onClick={toggle}>
+    {isDark ? <Brightness5 /> : <Brightness4 />}
+  </IconButton>;
+};
+ 
+export default ColorModeChangeButton;
