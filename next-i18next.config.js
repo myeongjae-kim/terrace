2cdc100f6613557ab804ef78ab5fd24b6de56f54
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-16 02:09:11 +0900

    fix next-i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   i18n: {
     defaultLocale: "ko",
     locales: ["ko", "en"],
+    localePath: path.resolve("./public/static/locales"),
   },
-  localePath: path.resolve("./public/static/locales"),
 };
