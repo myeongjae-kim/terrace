9ddc2aa7e4720b7fd037f7b016f90706c3581645
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-15 20:21:53 +0900

    remove unusing services

--- src/common/domain/service/CacheRenderingService.ts
+++
@@ -1,6 +0,0 @@
-import Optional from "optional-js";
-
-export interface CacheRenderingService {
-  findByPath(path: string): Optional<string>;
-  save(path: string, html: string): void;
-}
--- src/musings/domain/service/MusingService.ts
+++
@@ -1,5 +0,0 @@
-import { MusingResponseDto } from "src/musings/api/dto";
-
-export interface MusingService {
-  findAll(): Promise<MusingResponseDto[]>;
-}
