f28094f856f9142d00e3b0a3d88ae8eb5a699a95
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-16 03:38:50 +0900

    darkmode is default

--- src/common/domain/model/usePersistentDarkModePreferences.ts
+++ src/common/domain/model/usePersistentDarkModePreferences.ts
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { useMediaQuery } from "@material-ui/core";
-import { isServer } from "src/util";
+import {isServer} from "src/util";
 
 const persist = (key: string, value: boolean) => {
   if (!isServer()) {
@@ -21,7 +20,9 @@ const getPersisted = (key: string) => {
 let initialized = false;
 
 export const usePersistentDarkModePreference = (persistentKey: string): [boolean, () => void] => {
-  const [prefersDarkMode, setDarkModePreference] = React.useState(useMediaQuery("(prefers-color-scheme: dark)"));
+  const [prefersDarkMode, setDarkModePreference] = React.useState(
+    true // useMediaQuery("(prefers-color-scheme: dark)")
+  );
   const persisted = getPersisted(persistentKey);
 
   if (!initialized && persisted) {
