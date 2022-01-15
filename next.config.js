9ad5622755c88f5f5b8211e87932a5317f396b06
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-16 01:56:28 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
-  }
+  },
+  i18n
 };
