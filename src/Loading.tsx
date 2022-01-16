50be5c60be54f73fdf9119f549fdd6f0477f6fbc
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-16 21:45:13 +0900

    add poets

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -2,12 +2,42 @@ import RainbowText from "./RainbowText";
 import * as React from "react";
 import {CSSProperties} from "react";
 
+interface Poets {
+  title: string
+  phrase: string
+  poet: string
+}
+
+const poets: Poets[] = [{
+  title: "기다림",
+  phrase: "그대의 회답을 기다리지요.",
+  poet: "원태연"
+},{
+  title: "너를 기다리는 동안",
+  phrase: "마침내 나는 너에게 간다",
+  poet: "황지우"
+}, {
+  title: "너를 기다리는 동안",
+  phrase: "나는 너에게 가고 있다.",
+  poet: "황지우"
+}, {
+  title: "지루함",
+  phrase: "기다림이 없는 인생은 지루할 거다",
+  poet: "조병화"
+}, {
+  title: "기다려야지",
+  phrase: "궁금해도 기다려야지",
+  poet: "유경환"
+}];
+
 const Loading: React.FC<{style?: CSSProperties}> = ({style}) => {
+  const {title, phrase, poet} = poets[Math.floor(Math.random() * poets.length)];
+
   return <RainbowText
     line1=""
     line2={"..Loading.."}
-    line3="그대의 회답을 기다리지요."
-    line4="- 원태연, [기다림]"
+    line3={phrase}
+    line4={`- ${poet}, [${title}]`}
     style={style || {paddingTop: "calc(-100px + 30vh)"}}
   />;
 };
