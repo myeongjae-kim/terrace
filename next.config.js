442e5b3144d72b13fd5ddd3c8a5f121a735808f1
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-01-17 16:48:56 +0900

    remove module resolution config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const path = require('path');
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
-  i18n,
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  }
+  i18n
 };
