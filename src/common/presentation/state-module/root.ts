9820adbca52d59edcbe2b92e2dd3f26b9dd04d59
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-02-27 18:23:27 +0900

    [#117] remove redux-form

--- src/common/presentation/state-module/root.ts
+++ src/common/presentation/state-module/root.ts
@@ -1,11 +1,8 @@
 import {combineReducers,} from "redux";
-import {reducer as formReducer} from "redux-form";
 import {StateType} from "typesafe-actions";
 import * as commonModule from "./common";
 
 export const rootReducer = combineReducers({
-  form: formReducer,
-
   common: commonModule.reducer,
 });
 
