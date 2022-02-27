53ef06ec5336662b294b4aa2713375845f213a82
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-02-27 17:51:36 +0900

    [#117] remove musings redux module

--- src/common/presentation/state-module/root.ts
+++ src/common/presentation/state-module/root.ts
@@ -3,7 +3,6 @@ import { reducer as formReducer } from "redux-form";
 import { fork } from "redux-saga/effects";
 import * as blogModule from "src/blog/presentation/state-modules";
 import * as dailyModule from "src/daily/presentation/state-modules";
-import * as musingsModule from "src/musings/presentation/state-modules";
 import { StateType } from "typesafe-actions";
 import * as commonModule from "./common";
 import * as snackbarModule from "./snackbar";
@@ -15,13 +14,11 @@ export const rootReducer = combineReducers({
   snackbar: snackbarModule.reducer,
 
   blog: blogModule.reducer,
-  musings: musingsModule.reducer,
   daily: dailyModule.reducer,
 });
 
 export function* rootSaga() {
   yield fork(blogModule.saga);
-  yield fork(musingsModule.saga);
   yield fork(dailyModule.saga);
 }
 
