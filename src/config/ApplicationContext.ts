9fea96cc87bfbdda0d0679c3f2b2c1fbfb38b91e
Myeongjae Kim <dev@myeongjae.kim>
2022-03-08 23:23:28 +0900

    [#142] use inversify (#145)
    * [#142] use inversify for application context
    
    but failed to build because of initializing took so long
    
    * [#142] update package-lock.json

--- src/config/ApplicationContext.ts
+++ src/config/ApplicationContext.ts
@@ -1,53 +1,22 @@
 import {AboutGetUseCase} from "../about/application/port/incoming/AboutGetUseCase";
-import {AboutService} from "../about/application/AboutService";
-import {AboutInMemoryAdapter} from "../about/adapter/outgoing/AboutInMemoryAdapter";
 import {BlogGetUseCase} from "../blog/application/port/incoming/BlogGetUseCase";
-import {BlogService} from "../blog/application/BlogService";
-import {BlogPersistenceAdapter} from "../blog/adapter/outgoing/BlogPersistenceAdapter";
 import {BlogFindAllUseCase} from "../blog/application/port/incoming/BlogFindAllUseCase";
 import {BlogGetPrevOrNextUseCase} from "../blog/application/port/incoming/BlogGetPrevOrNextUseCase";
-import {DailyService} from "../daily/application/port/DailyService";
-import {DailyPersistenceAdapter} from "../daily/adapter/outgoing/DailyPersistenceAdapter";
 import {DailyGetUseCase} from "../daily/application/port/incoming/DailyGetUseCase";
 import {DailyFindAllUseCase} from "../daily/application/port/incoming/DailyFindAllUseCase";
-import {MusingService} from "../musing/application/MusingService";
-import {MusingPersistenceAdapter} from "../musing/adapter/outgoing/MusingPersistenceAdapter";
 import {MusingFindAllUseCase} from "../musing/application/port/incoming/MusingFindAllUseCase";
-import Axios, {AxiosInstance} from "axios";
-import {API_HOST} from "../common/constants/Constants";
+import {container, USE_CASES} from "./inversify";
 
 // context
 class ApplicationContext {
-  // dependencies
-  private readonly publicToken =
-    `5f5da4885cc60c4007a770e20bcb499584306df76f7024786943770d87d10ec647588ed508a328726c03144cecb04e65377865e992cf557c939
-8f280355f1b5d66816bd18b466c4c973d90a93c5a04b3635a518688b2e49c468eca9c92bf0098dc6851481cd51bc9d60b33c4b7c65e81885b6dd5399
-0b7e0397451b59cd000e6`.replace(/\n/g, "");
-
-  private readonly axiosInstance: AxiosInstance = Axios.create({
-    baseURL: API_HOST,
-    headers: {
-      "Authorization": "Bearer " + this.publicToken
-    }
-  });
-
-  // implementations
-  private readonly aboutService: AboutService = new AboutService(new AboutInMemoryAdapter());
-  private readonly blogService: BlogService = new BlogService(
-    new BlogPersistenceAdapter(this.axiosInstance),
-    new BlogPersistenceAdapter(this.axiosInstance),
-  );
-  private readonly dailyService: DailyService = new DailyService(new DailyPersistenceAdapter(this.axiosInstance));
-  private readonly musingService: MusingService = new MusingService(new MusingPersistenceAdapter(this.axiosInstance));
-
-  // interfaces
-  readonly aboutGetUseCase: AboutGetUseCase = this.aboutService;
-  readonly blogGetUseCase: BlogGetUseCase = this.blogService;
-  readonly blogFindAllUseCase: BlogFindAllUseCase = this.blogService;
-  readonly blogGetPrevOrNextUseCase: BlogGetPrevOrNextUseCase = this.blogService;
-  readonly dailyGetUseCase: DailyGetUseCase = this.dailyService;
-  readonly dailyFindAllUseCase: DailyFindAllUseCase = this.dailyService;
-  readonly musingFindAllUseCase: MusingFindAllUseCase = this.musingService;
+  // use cases
+  readonly aboutGetUseCase = container.get<AboutGetUseCase>(USE_CASES.AboutGetUseCase);
+  readonly blogGetUseCase = container.get<BlogGetUseCase>(USE_CASES.BlogGetUseCase);
+  readonly blogFindAllUseCase = container.get<BlogFindAllUseCase>(USE_CASES.BlogFindAllUseCase);
+  readonly blogGetPrevOrNextUseCase = container.get<BlogGetPrevOrNextUseCase>(USE_CASES.BlogGetPrevOrNextUseCase);
+  readonly dailyGetUseCase = container.get<DailyGetUseCase>(USE_CASES.DailyGetUseCase);
+  readonly dailyFindAllUseCase = container.get<DailyFindAllUseCase>(USE_CASES.DailyFindAllUseCase);
+  readonly musingFindAllUseCase = container.get<MusingFindAllUseCase>(USE_CASES.MusingFindAllUseCase);
 }
 
 export const applicationContext = new ApplicationContext();
---
+++ src/config/myAxiosRequestConfig.ts
@@ -0,0 +1,12 @@
+import {AxiosRequestConfig} from "axios";
+import {API_HOST} from "../common/constants/Constants";
+
+export const myAxiosRequestConfig: AxiosRequestConfig = {
+  baseURL: API_HOST,
+  headers: {
+    "Authorization": "Bearer " +
+      `5f5da4885cc60c4007a770e20bcb499584306df76f7024786943770d87d10ec647588ed508a328726c03144cecb04e65377865e992cf557c9
+398f280355f1b5d66816bd18b466c4c973d90a93c5a04b3635a518688b2e49c468eca9c92bf0098dc6851481cd51bc9d60b33c4b7c65e81885b6dd53
+990b7e0397451b59cd000e6`.replace(/\n/g, "")
+  }
+};
