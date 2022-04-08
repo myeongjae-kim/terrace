ca2252b4d51e1eebd2d1154a977f8f2be068b11f
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-04-08 18:47:54 +0900

    [#173] fix pagination link

--- src/common/view/presentation/components/organisms/MyPagination.tsx
+++ src/common/view/presentation/components/organisms/MyPagination.tsx
@@ -4,6 +4,7 @@ import {Link} from "src/common/view/presentation/components/molecules";
 import {useRouter} from "next/router";
 import {usePaginationHrefGenerator} from "src/util/usePaginationHrefGenerator";
 import {Pagination, PaginationItem} from "@mui/material";
+import {returnCreateLinkClickHandler} from "../../../../../util";
 
 interface Props {
   pagination: StrapiPagination
@@ -13,9 +14,11 @@ export const MyPagination = (props: Props) => {
   const {pagination} = props;
   const router = useRouter();
   const hrefGenerator = usePaginationHrefGenerator();
-  const onChange = React.useCallback((_: React.ChangeEvent<unknown>, page: number) => {
-    router.push(hrefGenerator(page)).then();
-  }, [hrefGenerator, router]);
+
+  const createLinkClickHandler = React.useMemo(() => returnCreateLinkClickHandler(router), [router]);
+  const onChange = React.useCallback((e: React.ChangeEvent<unknown>, page: number) => {
+    createLinkClickHandler(hrefGenerator(page))(e as unknown as React.MouseEvent<any, MouseEvent>);
+  }, [createLinkClickHandler, hrefGenerator]);
 
   return <Pagination
     page={pagination.page}
