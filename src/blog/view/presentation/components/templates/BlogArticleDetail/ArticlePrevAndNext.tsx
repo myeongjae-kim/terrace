0b28f28081f78026ee1c6845284b2ffc1d1a2780
김명재 Myeongjae Kim <dev@myeongjae.kim>
2022-04-30 02:38:18 +0900

    feat: conditional rendering for prev and next article

--- src/blog/view/presentation/components/templates/BlogArticleDetail/ArticlePrevAndNext.tsx
+++ src/blog/view/presentation/components/templates/BlogArticleDetail/ArticlePrevAndNext.tsx
@@ -1,60 +1,76 @@
 import { createStyles, makeStyles } from "@mui/styles";
 import * as React from "react";
 import { Link } from "src/common/view/presentation/components/molecules";
-import {BlogArticlePrevOrNext} from "../../../../../domain/BlogArticleDetailResponse";
-import {Button, Theme, Typography} from "@mui/material";
+import { BlogArticlePrevOrNext } from "../../../../../domain/BlogArticleDetailResponse";
+import { Button, Theme, Typography } from "@mui/material";
 
-const useEachStyles = makeStyles((theme: Theme) => createStyles({
-  eachContainer: {
-    margin: `${theme.spacing(1)} 0`,
-  },
-  label: {
-    marginBottom: theme.spacing(0.5),
-    userSelect: "none",
-  }
-}));
+const useEachStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    eachContainer: {
+      margin: `${theme.spacing(1)} 0`,
+    },
+    label: {
+      marginBottom: theme.spacing(0.5),
+      userSelect: "none",
+    },
+  })
+);
 
-
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  container: {
-    margin: `${theme.spacing(4)} 0`,
-    textAlign: "center"
-  },
-  hrUpper: {
-    marginBottom: theme.spacing(2)
-  },
-  hrLower: {
-    margin: `${theme.spacing(2)} 0`
-  }
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      margin: `${theme.spacing(4)} 0`,
+      textAlign: "center",
+    },
+    hrUpper: {
+      marginBottom: theme.spacing(2),
+    },
+    hrLower: {
+      margin: `${theme.spacing(2)} 0`,
+    },
+  })
+);
 
 interface Props {
   prev: BlogArticlePrevOrNext;
   next: BlogArticlePrevOrNext;
 }
 
-const Each = ({ label, article }: { label: string; article: BlogArticlePrevOrNext }) => {
+const Each = ({
+  label,
+  article,
+}: {
+  label: string;
+  article: BlogArticlePrevOrNext;
+}) => {
   const classes = useEachStyles();
 
-  return <div className={classes.eachContainer} hidden={!article.id}>
-    <Typography variant="subtitle2" className={classes.label}>{label}</Typography>
-    <Link href={article.uri}>{article.title}</Link>
-  </div>;
-
+  return (
+    <div className={classes.eachContainer} hidden={!article.id}>
+      <Typography variant="subtitle2" className={classes.label}>
+        {label}
+      </Typography>
+      <Link href={article.uri}>{article.title}</Link>
+    </div>
+  );
 };
 
 const ArticlePrevAndNext = ({ prev, next }: Props) => {
   const classes = useStyles();
-  return <div className={classes.container}>
-    <hr className={classes.hrUpper} />
-    <Each label='Next Article' article={next} />
-    <Each label='Previous Article' article={prev} />
-    <hr className={classes.hrLower} />
+  return (
+    <div className={classes.container}>
+      <hr className={classes.hrUpper} />
+      {next.title && <Each label="Next Article" article={next} />}
+      {prev.title && <Each label="Previous Article" article={prev} />}
+      <hr className={classes.hrLower} />
 
-    <Link href="/blog">
-      <Button variant="outlined" style={{ textTransform: "capitalize" }}>Article List</Button>
-    </Link>
-  </div>;
+      <Link href="/blog">
+        <Button variant="outlined" style={{ textTransform: "capitalize" }}>
+          Article List
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default ArticlePrevAndNext;
