9ea30037552dae585ae41a46a3608e3e8d1b7770
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-01-01 14:50:17 +0900

    fix to pass lint

--- src/common/view/presentation/components/organisms/AceWrapper.tsx
+++ src/common/view/presentation/components/organisms/AceWrapper.tsx
@@ -4,9 +4,8 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/theme-cobalt";
 import "ace-builds/src-noconflict/ext-language_tools";
-import { IAceEditorProps } from "react-ace/src/ace";
 
-const AceWrapper = (props: IAceEditorProps): JSX.Element => {
+const AceWrapper = (props: React.ComponentProps<typeof AceEditor>): JSX.Element => {
   return <AceEditor
     {...props}
     mode="markdown"
