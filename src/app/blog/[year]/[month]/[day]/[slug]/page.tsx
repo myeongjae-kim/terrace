506b6c1f2fa0cdc4ce867d2e1ec0dc4cd29c83cc
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-07-10 01:01:17 +0900

    feat: render prev and next article link of blog article

--- src/app/blog/[year]/[month]/[day]/[slug]/page.tsx
+++ src/app/blog/[year]/[month]/[day]/[slug]/page.tsx
@@ -5,10 +5,14 @@ import { formatDate } from '@/app/common/domain/model/formatDate';
 import Link from 'next/link';
 import MarkdownRenderer from '@/app/common/components/MarkdownRenderer';
 import Comment from '@/app/common/components/Comment';
+import { toSlug } from '@/app/common/domain/model/toSlug';
 
 const BlogArticlePage = async (props: PageProps<{ slug: string }>): Promise<JSX.Element> => {
   const article = await blogPersistenceAdapter.getBySlug(props.params.slug);
-  const commentIdentifier = `daily/${formatDate(article.created_at, '/')}/${article.slug}`;
+  const [prev, next] = await Promise.all(
+    [blogPersistenceAdapter.getPrevOf, blogPersistenceAdapter.getNextOf].map((f) => f(article.seq)),
+  );
+  const commentIdentifier = `blog/${formatDate(article.created_at, '/')}/${article.slug}`;
 
   return (
     <main className={'w-full max-w-[800px]'}>
@@ -20,6 +24,35 @@ const BlogArticlePage = async (props: PageProps<{ slug: string }>): Promise<JSX.
       </div>
       <div className={'m-4'}>
         <MarkdownRenderer className={'leading-[1.8]'} markdown={article.content} />
+        <div className={'text-sm'}>
+          <hr />
+          <div className={'my-4 flex flex-col gap-1.5 text-center'}>
+            {!!next.id && (
+              <>
+                <div>Next Article</div>
+                <Link href={`/blog/${toSlug(next)}`}>{next.title}</Link>
+              </>
+            )}
+            {!!prev.id && (
+              <>
+                <div className={'mt-2'}>Previous Article</div>
+                <Link href={`/blog/${toSlug(prev)}`}>{prev.title}</Link>
+              </>
+            )}
+          </div>
+          <hr />
+          <div className={'my-4 flex justify-center'}>
+            <Link href={'/blog'}>
+              <button
+                type="button"
+                className="rounded-lg border border-gray-200 px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              >
+                Article List
+              </button>
+            </Link>
+          </div>
+        </div>
+
         <Comment identifier={commentIdentifier} />
       </div>
     </main>
