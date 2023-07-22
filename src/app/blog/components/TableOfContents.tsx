dbc2b1fe88dd24a882a1c6e241a4fe9c4b714d61
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-07-23 05:31:21 +0900

    feat: add toc button

--- src/app/blog/components/TableOfContents.tsx
+++ src/app/blog/components/TableOfContents.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { TOC_ID } from '@/app/common/domain/model/constants';
-import parser from 'node-html-parser';
+import { HTMLElement } from 'node-html-parser';
 
 type Props = {
-  html: string;
+  htmlElement: HTMLElement;
 };
 
 type Heading = {
@@ -43,14 +43,14 @@ const render = (headings: Heading[], curr = 0): string => {
   )}`;
 };
 
-const TableOfContents = ({ html }: Props): JSX.Element => {
-  const headings: Heading[] = [...parser(html).querySelectorAll('h1, h2, h3, h4, h5, h6')].map(
-    (block) => ({
+const TableOfContents = ({ htmlElement }: Props): JSX.Element => {
+  const headings: Heading[] = htmlElement
+    .querySelectorAll('h1, h2, h3, h4, h5, h6')
+    .map((block) => ({
       depth: parseInt(block.tagName[1]),
       id: block.id,
       innerHTML: block.innerHTML,
-    }),
-  );
+    }));
 
   return headings.length > 0 ? (
     <>
--- src/app/common/components/MarkdownRenderer.tsx
+++ src/app/common/components/MarkdownRenderer.tsx
@@ -2,6 +2,7 @@ import { marked } from 'marked';
 import { markedHighlight } from 'marked-highlight';
 import Prism from 'prismjs';
 import * as React from 'react';
+import { forwardRef } from 'react';
 import { mangle } from 'marked-mangle';
 import { gfmHeadingId } from 'marked-gfm-heading-id';
 
@@ -15,8 +16,9 @@ import 'prismjs/components/prism-tsx.min.js'; // prism-jsx를 import해야 에�
 import 'prismjs/components/prism-typescript.min.js';
 import 'prismjs/components/prism-vim.min.js';
 import 'prismjs/components/prism-yaml.min.js';
-import { forwardRef } from 'react';
 import TableOfContents from '@/app/blog/components/TableOfContents';
+import parser from 'node-html-parser';
+import { TOC_ID } from '@/app/common/domain/model/constants';
 
 type Props = React.ComponentProps<'div'> & {
   markdown: string;
@@ -43,14 +45,33 @@ const MarkdownRenderer = (
 ): JSX.Element => {
   const html = marked(markdown);
 
+  const parsedForContents = parser(html);
+  parsedForContents.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((block) => {
+    block.classList.add('flex');
+    block.classList.add('gap-1');
+    block.classList.add('items-start');
+    block.innerHTML = `${block.innerHTML} <a href="#${block.id}" onClick="navigator.clipboard.writeText(window.location.origin + window.location.pathname + '#${block.id}')">
+  <span class="material-icons cursor-pointer select-none" style="font-size: 1.2em">
+    link
+  </span>
+</a>
+  <span class="flex-1"></span>
+  <a href="#${TOC_ID}">
+    <span class="material-icons cursor-pointer select-none opacity-50" style="font-size: 1em">
+      toc
+    </span>
+  </a>
+`;
+  });
+
   return (
     <>
-      {enableToc && <TableOfContents html={html} />}
+      {enableToc && <TableOfContents htmlElement={parser(html)} />}
       <div
         ref={ref}
         style={{ overflowWrap: 'anywhere', ...style }}
         {...props}
-        dangerouslySetInnerHTML={{ __html: html }}
+        dangerouslySetInnerHTML={{ __html: parsedForContents.innerHTML }}
       ></div>
     </>
   );
