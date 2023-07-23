d4b25473c7049f6a5cf9ee72203538a232da2027
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-07-23 15:17:03 +0900

    feat: use cva

--- src/app/common/components/Button.tsx
+++ src/app/common/components/Button.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { cva, VariantProps } from 'class-variance-authority';
 
-type Props = React.ComponentProps<'button'> & {
-  size?: 'xs' | 'base';
-  additionalClassName?: string;
-};
+const buttonVariants = cva(
+  [
+    'rounded-lg bg-blue-700 font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300',
+  ],
+  {
+    variants: {
+      size: {
+        xs: 'px-3 py-2 text-xs',
+        base: 'px-5 py-2.5 text-sm',
+      },
+    },
+    defaultVariants: {
+      size: 'base',
+    },
+  },
+);
+
+type Props = React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    additionalClassName?: string;
+  };
 
 const Button = ({ size, additionalClassName, ...props }: Props): JSX.Element => {
   return (
     <button
       type="button"
-      className={twMerge(
-        'rounded-lg bg-blue-700 font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-        clsx({
-          ['px-3 py-2 text-xs']: size === 'xs',
-          ['px-5 py-2.5 text-sm']: size === 'base',
-        }),
-        additionalClassName,
-      )}
+      className={twMerge(buttonVariants({ size }), additionalClassName)}
       {...props}
     />
   );
