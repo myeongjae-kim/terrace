f7ef750fe2c815f439467c34b595e1255cd9b511
Myeongjae Kim (김명재) <dev@myeongjae.kim>
2025-12-25 00:59:19 +0900

    feat: OWNER_SUB 추가

--- src/app/common/env.ts
+++ src/app/common/env.ts
@@ -4,10 +4,12 @@ export const ENV = cleanEnv(
   {
     NEXT_PUBLIC_GOOGLE_LOGIN_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_LOGIN_CLIENT_ID,
     GOOGLE_LOGIN_CLIENT_SECRET: process.env.GOOGLE_LOGIN_CLIENT_SECRET,
+    OWNER_SUB: process.env.OWNER_SUB,
   },
   {
     NEXT_PUBLIC_GOOGLE_LOGIN_CLIENT_ID: str(),
     // client일 떄만 optional
     GOOGLE_LOGIN_CLIENT_SECRET: str(typeof window !== 'undefined' ? { default: '' } : undefined),
+    OWNER_SUB: str(),
   },
 );
