52de5f9a56d0af78f56e48bfbc39f0173847bef3
Myeongjae Kim <dev@myeongjae.kim>
2026-07-07 23:09:55 +0900

    chore: add infra

---
+++ lint-staged.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  '**/*.(mts|ts|tsx|js)': (filenames) => [
+    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
+  ],
+};
+
+export default config;
