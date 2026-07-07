1fda0967a6bb7b3136e3f8474f4850212656aa74
Myeongjae Kim <dev@myeongjae.kim>
2026-07-08 00:45:48 +0900

    chore: exclude biome from formatting

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,17 @@
 const config = {
-  '**/*.(mts|ts|tsx|js)': (filenames) => [
-    `pnpx biome check --fix ${filenames.join(' ')}`,
-    `pnpx prettier --write ${filenames.join(' ')}`,
-  ],
+  "**/*.(mts|ts|tsx|js)": (filenames) => {
+    const filteredFilenames = filenames.filter(
+      (filename) => !filename.endsWith("src/routeTree.gen.ts"),
+    );
+
+    if (filteredFilenames.length === 0) {
+      return [];
+    }
+
+    return [
+      `pnpx biome check --fix ${filteredFilenames.join(" ")}`,
+    ];
+  },
 };
 
 export default config;
